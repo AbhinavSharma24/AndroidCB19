76a120ec7a2c6cd0a780a62dbe56a2215dc4167f
abhinav <abhinavsharma24999@gmail.com>
2019-07-19 14:36:28 +0530

    Android Camera with ImageCapture

--- CameraActivity/app/src/main/java/com/example/cameraactivity/MainActivity.kt
+++ CameraActivity/app/src/main/java/com/example/cameraactivity/MainActivity.kt
@@ -2,16 +2,19 @@ package com.example.cameraactivity
 
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Matrix
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Rational
 import android.util.Size
+import android.view.Surface
+import android.view.View
 import android.view.ViewGroup
-import androidx.camera.core.CameraX
-import androidx.camera.core.Preview
-import androidx.camera.core.PreviewConfig
+import android.widget.Toast
+import androidx.camera.core.*
 import androidx.core.app.ActivityCompat
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,6 +22,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        captureBtn.visibility = View.GONE
         requests()
     }
 
@@ -36,16 +40,43 @@ class MainActivity : AppCompatActivity() {
         }
         else {
             textureView.post{
-                startCamera()
+                camera.setOnClickListener {
+                    startCamera()
+                    captureBtn.visibility = View.VISIBLE
+                    camera.visibility = View.GONE
+                }
             }
         }
 
     }
 
     private fun startCamera(){
+        //to capture image on button click
+        val imageCaptureConfig = ImageCaptureConfig.Builder().apply{
+            setTargetAspectRatio(Rational(1,1))
+            setCaptureMode(ImageCapture.CaptureMode.MIN_LATENCY)
+        }.build()
+
+        val imageCapture = ImageCapture(imageCaptureConfig)
+        captureBtn.setOnClickListener{
+            val file = File(externalMediaDirs.first(),"${System.currentTimeMillis()}.jpg")
+            imageCapture.takePicture(file,object : ImageCapture.OnImageSavedListener{
+                override fun onImageSaved(file: File) {
+                    Toast.makeText(this@MainActivity,"Photo Captured ${file.absolutePath}",Toast.LENGTH_LONG).show()
+                }
+                override fun onError(useCaseError: ImageCapture.UseCaseError, message: String, cause: Throwable?) {
+                    Toast.makeText(this@MainActivity,"Error Capturing $message",Toast.LENGTH_LONG).show()
+                }
+
+            })
+        }
+
+
+        //to get the preview
         val previewConfig = PreviewConfig.Builder().apply {
             setTargetResolution(Size(1080, 1080))
             setTargetAspectRatio(Rational(1, 1))
+            /*setLensFacing(CameraX.LensFacing.FRONT)     //to open front camera by force*/
         }.build()
 
         val preview = Preview(previewConfig)
@@ -54,10 +85,28 @@ class MainActivity : AppCompatActivity() {
             val parent = textureView.parent as ViewGroup
             parent.removeView(textureView)
             parent.addView(textureView,0)
-
+            updateView()
             textureView.surfaceTexture = it.surfaceTexture
         }
-        CameraX.bindToLifecycle(this,preview)
+        CameraX.bindToLifecycle(this,preview,imageCapture)
+
+    }
+
+    private fun updateView(){
+        val matrix = Matrix()
+
+        val centerX = textureView.width/2f
+        val centerY = textureView.height/2f
+
+        val rotation = when(textureView.display.rotation){
+            Surface.ROTATION_0 -> 0
+            Surface.ROTATION_90 -> 90
+            Surface.ROTATION_180 -> 180
+            Surface.ROTATION_270 -> 270
+            else -> return
+        }
+        matrix.postRotate(-rotation.toFloat(),centerX,centerY)
+        textureView.setTransform(matrix)
     }
 }
 

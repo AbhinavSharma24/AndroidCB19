3afb83f93b05581809510218eddb0cfbdd2129f0
abhinav <abhinavsharma24999@gmail.com>
2019-07-19 12:35:29 +0530

    Android IMBD Clone

--- IMDBclone/app/src/main/java/com/example/imdbclone/CastDetails.kt
+++ IMDBclone/app/src/main/java/com/example/imdbclone/CastDetails.kt
@@ -3,6 +3,7 @@ package com.example.imdbclone
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_castdetails.*
@@ -11,21 +12,49 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import android.view.ViewTreeObserver
+import android.animation.ValueAnimator
+import android.animation.Animator
+import kotlinx.android.synthetic.main.app_bar_main.*
+
 
 @SuppressLint("Registered")
 class CastDetails : AppCompatActivity() {
+    //lateinit var mAnimator: ValueAnimator
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_castdetails)
 //        setSupportActionBar(toolbar)
-//        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
         val pos = intent.getIntExtra("castID",0)
         val retrofitClient = Retrofit.Builder()
             .baseUrl("https://api.themoviedb.org/3/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
+        /*expandable.viewTreeObserver.addOnPreDrawListener(object : ViewTreeObserver.OnPreDrawListener {
+            override fun onPreDraw(): Boolean {
+                expandable.viewTreeObserver.removeOnPreDrawListener(this)
+                expandable.visibility = View.GONE
+
+                val widthSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED)
+                val heightSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED)
+                expandable.measure(widthSpec, heightSpec)
+
+                mAnimator = slideAnimator(0, expandable.measuredHeight)
+                return true
+            }
+        })
+
+        seemore.setOnClickListener {
+            if (expandable.visibility == View.GONE) {
+                expand()
+            } else {
+                collapse()
+            }
+        }*/
 
         val service = retrofitClient.create(GithubService::class.java)
 
@@ -53,9 +82,13 @@ class CastDetails : AppCompatActivity() {
             ) {
                 runOnUiThread {
                     //toolbar.title=response.body()?.name
-                    val age = 2019-response.body()?.birthday?.substring(0,4)!!.toInt()
-                    tv.text="\n\nAge: "+age+"\nBirthPlace: "+response.body()?.place_of_birth+
-                            "\nBiography\n"+response.body()?.biography
+                    if(response.body()?.birthday != null && response.body()?.place_of_birth != null && response.body()?.biography != null) {
+                        val age = 2019 - response.body()?.birthday?.substring(0, 4)!!.toInt()
+                        tv.text = "\n\nAge: " + age + "\nBirthPlace: " + response.body()?.place_of_birth +
+                                "\nBiography\n" + response.body()?.biography
+                    }else{
+                        tv.text = "No Information Available"
+                    }
                 }
             }
         })
@@ -77,4 +110,37 @@ class CastDetails : AppCompatActivity() {
 
 
     }
+
+    /*private fun expand() {
+        expandable.visibility = View.VISIBLE
+        mAnimator.start()
+    }
+
+    private fun collapse() {
+        val finalHeight = expandable.height
+        val mAnimator = slideAnimator(finalHeight, 0)
+        mAnimator.addListener(object : Animator.AnimatorListener {
+            override fun onAnimationEnd(animator: Animator) {
+                //Height=0, but it set visibility to GONE
+                expandable.visibility = View.GONE
+            }
+            override fun onAnimationStart(animator: Animator) {}
+            override fun onAnimationCancel(animator: Animator) {}
+            override fun onAnimationRepeat(animator: Animator) {}
+        })
+        mAnimator.start()
+    }
+
+    private fun slideAnimator(start: Int, end: Int): ValueAnimator {
+        val animator = ValueAnimator.ofInt(start, end)
+        animator.addUpdateListener { valueAnimator ->
+            val value = valueAnimator.animatedValue as Int
+            val layoutParams = expandable.layoutParams
+            layoutParams.height = value
+            expandable.layoutParams = layoutParams
+        }
+        return animator
+    }*/
+
+
 }
